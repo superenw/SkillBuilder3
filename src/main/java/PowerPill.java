34f4a911d46bc04109c44d109351bfad9f2f4b61
. . <bracewellsando@gmail.com>
2026-09-19 23:08:58 -0400

    complete assignment

--- src/main/java/PowerPill.java
+++ src/main/java/PowerPill.java
@@ -1,4 +1,3 @@
-
 /**
  * This class defines how power pills behave in
  * a game.  The purpose of a power pill is to simply
@@ -9,30 +8,58 @@
  */
 public class PowerPill
 {
-    // TODO - replace this line with instruction from step 1
+    // constant
+    public static final int DEFAULT_POWER = 10;
 
     // instance variables
-    // TODO - replace this line with instruction from step 2
-    // TODO - replace this line with instruction from step 3
+    private int power;
+    private String name;
 
     // constructors
 
-    // TODO - replace this line with instruction from step 4
+    public PowerPill(String name)
+    {
+        this.name = name;
+        this.power = DEFAULT_POWER;
+    }
 
-    // TODO - replace this line with instruction from step 5
+    public PowerPill(String name, int power)
+    {
+        this.name = name;
+        this.power = power;
+    }
 
 
     // accessor methods
 
-    // TODO - replace this line with instruction from step 6
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getPower()
+    {
+        return power;
+    }
 
 
     // mutator methods
 
-    // TODO - replace this line with instruction from step 7
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setPower(int power)
+    {
+        this.power = power;
+    }
+
 
     // toString method
 
-    // TODO - replace this line with instruction from step 8
-
+    public String toString()
+    {
+        return "PowerPill " + name + " = " + power;
+    }
 }
